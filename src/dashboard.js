e98bc7903a6d2f48db5d89165947934406ee7d73
Scott <scott.ti312@gmail.com>
2022-08-27 17:52:38 -0700

    Move 'add project' and 'add todo' to separate functions

--- src/dashboard.js
+++ src/dashboard.js
@@ -6,73 +6,14 @@ export function dashboard() {
   let projects = document.createElement('ul');
   let todosContainer = document.createElement('div');
   let todos = document.createElement('ul');
-  let addTodo = document.createElement('button');
-  let addProject = document.createElement('button');
   // let { demoAccount, inbox } = createDemo();
   // localStorage.clear();
   let demoAccount = JSON.parse(localStorage.getItem('user'));
   console.log(demoAccount);
   let inbox = demoAccount.projects[0];
   displayTodos(inbox, todos);
-  addTodo.innerHTML = 'Add new task';
-
-  addTodo.addEventListener('click', () => {
-    todos.removeChild(addTodo);
-    let todoInput = document.createElement('input');
-    let todoSubmit = document.createElement('button');
-    let todoDisplay = document.createElement('li');
-    todoSubmit.innerHTML = 'Submit';
-    todoSubmit.id = 'submit';
-    todoSubmit.addEventListener('click', () => {
-      let newTodo = todo(todoInput.value);
-      inbox.todos.push(newTodo);
-      localStorage.setItem('user', JSON.stringify(demoAccount));
-      todoDisplay.innerHTML = newTodo.title;
-      todos.append(todoDisplay);
-      todos.append(addTodo);
-      todos.removeChild(todoInput);
-      todos.removeChild(todoSubmit);
-      todos.appendChild(addTodo);
-    });
-    todos.append(todoInput);
-    todos.append(todoSubmit);
-    todoInput.addEventListener('keypress', function(event) {
-      if (event.key == 'Enter') {
-        event.preventDefault();
-        document.getElementById('submit').click();
-      }
-    });
-  });
-  
-  addProject.innerHTML = 'Add new project';
-  addProject.addEventListener('click', () => {
-    projects.removeChild(addProject);
-    let projectInput = document.createElement('input');
-    let projectSubmit = document.createElement('button');
-    let projectDisplay = document.createElement('li');
-    projectSubmit.innerHTML = 'Submit';
-    projectSubmit.id = 'submit';
-    projectSubmit.addEventListener('click', () => {
-      let newProject = project(projectInput.value);
-      demoAccount.projects.push(newProject);
-      localStorage.setItem('user', JSON.stringify(demoAccount));
-      projectDisplay.innerHTML = newProject.title;
-      projects.append(projectDisplay);
-      projects.append(addProject);
-      projects.removeChild(projectInput);
-      projects.removeChild(projectSubmit);
-      projects.appendChild(addProject);
-    });
-    projects.append(projectInput);
-    projects.append(projectSubmit);
-    projectInput.addEventListener('keypress', function(event) {
-      if (event.key == 'Enter') {
-        event.preventDefault();
-        document.getElementById('submit').click();
-      }
-    });
-  });
-
+  let addTodo = addNewTodo(inbox, todos, demoAccount);
+  let addProject = addNewProject(projects, demoAccount);
   displayProjects(demoAccount, projects);
   projects.append(addProject);
   todos.append(addTodo);
@@ -105,10 +46,68 @@ function displayProjects(demoAccount, sidebar) {
   }
 }
 
-function addTodo(inbox, todos) {
-  let todoDisplay = document.createElement('li');
-  let newTodo = todo()
-  inbox.addTodo(newTodo);
-  todoDisplay.innerHTML = newTodo.title;
-  todos.append(todoDisplay);
+function addNewTodo(inbox, todos, demoAccount) {
+  let addTodo = document.createElement('button');
+  addTodo.innerHTML = 'Add new task';
+  addTodo.addEventListener('click', () => {
+    todos.removeChild(addTodo);
+    let todoInput = document.createElement('input');
+    let todoSubmit = document.createElement('button');
+    let todoDisplay = document.createElement('li');
+    todoSubmit.innerHTML = 'Submit';
+    todoSubmit.id = 'submit';
+    todoSubmit.addEventListener('click', () => {
+      let newTodo = todo(todoInput.value);
+      inbox.todos.push(newTodo);
+      localStorage.setItem('user', JSON.stringify(demoAccount));
+      todoDisplay.innerHTML = newTodo.title;
+      todos.append(todoDisplay);
+      todos.append(addTodo);
+      todos.removeChild(todoInput);
+      todos.removeChild(todoSubmit);
+      todos.appendChild(addTodo);
+    });
+    todos.append(todoInput);
+    todos.append(todoSubmit);
+    todoInput.addEventListener('keypress', function(event) {
+      if (event.key == 'Enter') {
+        event.preventDefault();
+        document.getElementById('submit').click();
+      }
+    });
+  })
+  return addTodo;
+}
+
+function addNewProject(projects, demoAccount) {
+  let addProject = document.createElement('button');
+  addProject.innerHTML = 'Add new project';
+  addProject.addEventListener('click', () => {
+    projects.removeChild(addProject);
+    let projectInput = document.createElement('input');
+    let projectSubmit = document.createElement('button');
+    let projectDisplay = document.createElement('li');
+    projectSubmit.innerHTML = 'Submit';
+    projectSubmit.id = 'submit';
+    projectSubmit.addEventListener('click', () => {
+      let newProject = project(projectInput.value);
+      demoAccount.projects.push(newProject);
+      localStorage.setItem('user', JSON.stringify(demoAccount));
+      projectDisplay.innerHTML = newProject.title;
+      projects.append(projectDisplay);
+      projects.append(addProject);
+      projects.removeChild(projectInput);
+      projects.removeChild(projectSubmit);
+      projects.appendChild(addProject);
+    });
+    projects.append(projectInput);
+    projects.append(projectSubmit);
+    projectInput.addEventListener('keypress', function(event) {
+      if (event.key == 'Enter') {
+        event.preventDefault();
+        document.getElementById('submit').click();
+      }
+    });
+  });
+  return addProject;
 }
